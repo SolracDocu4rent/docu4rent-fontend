0c4c0a66942c27ff7df8734d0b5bacedd82f9317
Sanguña <solrac3658@Sangunas-MacBook-Air.local>
2024-06-03 08:17:03 -0400

    add home

---
+++ src/app/home/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import HomePage from "@/components/home/homePage";
+// import WhiteBackground from "./Images/Backgrounds/WhiteBackground.png";
+
+import Docu4rentNavbar from "@/components/reusables/Docu4rentNavbar";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center text-[#121212]">
+      <Docu4rentNavbar activeHomeValue />
+      <HomePage />
+    </main>
+  );
+}
