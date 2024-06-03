2bcc91e717a89bea15af4e556826e21c8035409b
Sanguña <solrac3658@Sangunas-MacBook-Air.local>
2024-06-03 08:15:02 -0400

    delete components

--- src/Components/Wrappers/layout-wrapper.component.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { ReactNode } from 'react';
-import { RouteGuard } from '@/components/security/route-guard.component';
-
-
-
-export const LayoutWrapperComponent = ({ children }: { children: ReactNode }) => {
-  return <RouteGuard>{children}</RouteGuard>;
-};
